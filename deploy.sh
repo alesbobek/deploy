6d487261553e2b65d01297c0f7d8f4f5d41e5ae0
Aleš Bobek <30588473+alesbobek@users.noreply.github.com>
2018-08-31 14:10:32 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
-apt-get -y update
-apt-get install \
-     apt-transport-https \
+apt-get update
+apt-get -y install apt-transport-https \
      ca-certificates \
      curl \
      gnupg2 \
      software-properties-common \
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+     lsb-release
+
+curl -fs https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 
-apt-get -y update && apg-get -y install docker-ce
+apt-get update && apt-get -y install docker-ce
 docker run -d --restart="always" -p 80:80 yeasy/simple-web
